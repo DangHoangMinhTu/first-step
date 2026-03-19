7dc4fab038dcbc07e03642de9cb4da5cc12ecdcd
DangHoangMinhTu <dangiehoang161107@gmail.com>
2026-03-20 00:38:38 +0700

    Update hoc_codec.cpp

--- hoc_codec.cpp
+++ hoc_codec.cpp
@@ -1,6 +1,15 @@
-#include <stdio.h>
-int main()
-{
-    printf("hello world");
-    return 0;
-}
+"""để xem digit(0-9) xuất hiện ở kí tự thứ mấyyy(PYTHON NHA)"""
+
+strings = input("enter the sentence: ")
+found = False
+position = 0
+while not found and position < len(strings):
+    if strings[position].isdigit() :
+        found=True
+    else:
+        position=position + 1
+
+if found:
+    print("first ouccurs at position", position)
+else:
+    print("the string does not contant a digit.")
